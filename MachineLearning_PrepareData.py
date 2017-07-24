89ecffbf8dbdee743aaabbf90c7743380490c2b6
Dilek Celik <dilekcell40@gmail.com>
2017-07-24 17:53:26 +0100

    Prepare Data For Machine Learning
    1. Rescale data.
    2. Standardize data.
    3. Normalize data.
    4. Binarize data.

---
+++ MachineLearning_PrepareData.py
@@ -0,0 +1,65 @@
+#Prepare Data For Machine Learning
+
+
+# Rescale data (between 0 and 1)
+from pandas import read_csv
+from numpy import set_printoptions
+from sklearn.preprocessing import MinMaxScaler
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = read_csv(url, names=names)
+array = dataframe.values
+
+# separate array into input and output components
+X = array[:,0:8]
+Y = array[:,8]
+scaler = MinMaxScaler(feature_range=(0, 1))
+rescaledX = scaler.fit_transform(X)
+# summarize transformed data
+set_printoptions(precision=3)
+print(rescaledX[0:5,:])
+
+# Standardize data (0 mean, 1 stdev)
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler().fit(X)
+rescaledX = scaler.transform(X)
+# summarize transformed data
+set_printoptions(precision=3)
+print(rescaledX[0:5,:])
+
+
+# Normalize data (length of 1)
+from sklearn.preprocessing import Normalizer
+scaler = Normalizer().fit(X)
+normalizedX = scaler.transform(X)
+# summarize transformed data
+set_printoptions(precision=3)
+print(normalizedX[0:5,:])
+
+
+#Binarize Data (Make Binary) BINARIZATION
+from sklearn.preprocessing import Binarizer
+binarizer = Binarizer(threshold=0.0).fit(X)
+binaryX = binarizer.transform(X)
+# summarize transformed data
+set_printoptions(precision=3)
+print(binaryX[0:5,:])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
